ba0539e9f21f39f83a1babcbf57dc5cf5c8f8422
Renato Jozinovic <94168180+renatoreli@users.noreply.github.com>
2021-11-18 23:25:44 +0100

    update

--- zadaci_pred_oop.py
+++ zadaci_pred_oop.py
@@ -24,7 +24,11 @@ print (lista_string)
 # %%
 #3.  Napiši program koji upisuje bodove n plesnih natjecanja. Ispiši zbroj svih bodova tako da odbaciš 
 #    najbolji i najlošiji rezultat.
-
+bodovi=[22,33,44,55,66]
+bodovi.remove(min(bodovi))
+bodovi.remove(max(bodovi))
+print(bodovi)
+print(sum(bodovi))
 
 
 #%%
@@ -38,13 +42,19 @@ print(kvadrat)
 
 lista=[1,2,3,"a","b","c"]
 brojevi=[]
-for item in lista:
-    for subitem in item.split():
-        if(subitem.isdigit()):
-            brojevi.append(subitem)
+string=[]
+for i in lista:
+    if type(i)== str:
+        string.append(i)
+    elif type(i) == int or type(i) == float:
+        brojevi.append(i)
 print(brojevi)
+print(string)
 # %%
-#6
+#6  S tipkovnice učitavajte cijele brojeve. Prvi upisani broj može biti bilo koji cijeli broj. Učitavanje ponavljati dok god je upisani broj strogo
+#veći od prethodno upisanog broja. Ispisati sumu svih učitanih brojeva
+#osim broja zbog kojeg je prekinuto učitavanje.
+
 
 def _sum_all_nums():
     tmp_a = 0
@@ -62,7 +72,9 @@ def _sum_all_nums():
 _sum_all_nums()
 
 #%%
-#7
+#7 Učitajte s tipkovnice 2 niza znakova, svaki od tih nizova znakova
+#spremite u zasebnu varijablu. Ispišite indekse na kojima se pojavljuju
+#ista slova neovisno o veličini ('a' i 'A' tretirati jednako)."""
 a="lagan"
 b="oblatna"
 
@@ -72,7 +84,11 @@ for index,char in enumerate(b):
     for index_2,char_2 in enumerate(a):
         print(index_2,char_2)
 #%%
-#8
+#8 Napišite program koji s tipkovnice učitava proizvoljni cijeli 
+#troznamenkasti broj. Ako učitani broj nije troznamenkasti, ispišite 
+#poruku o greški i prekinite daljnje izvođenje programa. U slučaju da 
+#je učitani broj ispravan, ispišite prvi sljedeći troznamenkasti 
+#palindrom. Na primjer, ako je učitani broj 120, prvi sljedeći palindrom  je 121.
 def print_next_palindrom(str_number):
     if str_number == str_number[::-1]:
         print("Palindrom je")
@@ -86,7 +102,8 @@ a =input("unesi broj: ")
 if len(a) ==1:
     print_next_palindrom(a)
 #%%
-#9
+#9 Napišite program koji učitava cijele brojeve sve dok je unesena 
+#vrijednost veća od 0. Pronađite koji od učitanih brojeva ima najmanju sumu znamenki te ispišite taj broj i sumu."
 num_list=[]
 while True:
     a = int(input("Unesi broj"))
@@ -108,7 +125,18 @@ for number in num_list:
     #print(num_sum)
 print(min_sum)
 #%%
-#11
+#10. S tipkovnice učitajte proizvoljni niz znakova. Kreirajte novi niz znakova koji će sadržavati naizmjence velika i mala slova iz ulaznog 
+#niza, redom kako se pojavljuju u ulaznom nizu: prvo veliko slovo u ulaznom nizu, prvo sljedeće malo slovo u nastavku ulaznog niza, 
+#prvo sljedeće veliko slovo u nastavku ulaznog niza itd. Novokreirani  niz ispišite na zaslon. U nastavku se nalazi primjer
+
+
+
+
+#%%
+
+#11 apišite program koji s tipkovnice učitava cijeli broj n iz intervala [3,  20]. U slučaju da je unesena vrijednost neispravna, ispisati prikladnu 
+#poruku na ekran te zatražiti ponovni unos cijelog broja. Nakon učitane vrijednosti n, učitajte n parova cijelih brojeva. Nakon što je n
+#parova brojeva učitano, ispišite parove brojeva koji imaju najveću sumu.
 
 def insert_numbers_into_all_pairs():
      pairs = []
@@ -134,12 +162,38 @@ while True:
      print(max_sum, all_pairs[num_index])
 
 #%%
-#12
+#12 Napišite program koji s tipkovnice učitava proizvoljni niz znakova. 
+#Nad učitanim nizom znakova napravite analizu je li taj niz palindrom 
+#ili nije. Niz je palindrom ako se isto čita slijeva nadesno ili pak zdesna nalijevo.
 string=input(("Unesi string"))
 if(string==string[::-1]):
       print("Ovo je palindrom")
 else:
       print("Nije palindrome")
+#%%
+#13 Napišite program koji će inicijalizirati varijablu n na proizvoljnu 
+#cjelobrojnu vrijednost. Vrijednost varijable n neka predstavlja red 
+#tablice. Ispisati tablicu veličine n redaka i n stupaca. Vrijednost 1 
+#neka se nalazi na glavnoj dijagonali, a vrijednost 0 na svim ostalim mjestima. U nastavku slijedi primjer za n=5:
+#1 0 0 0 0
+#0 1 0 0 0
+#0 0 1 0 0
+#0 0 0 1 0
+#0 0 0 0 1
+
+n=int(input("Unesi broj "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j==i:
+            print(1,end="")
+        else:
+            print("0",end="")
+    print()
+
+
+
+
+
 #%%
 #15
 x=int(input("Unesi prvi broj"))
@@ -148,15 +202,97 @@ for num in range(x, y + 1):
       if num % 2 != 0:
         print(num, end = " ")
 #%%
-#19
+#16 Napišite program koji sadrži varijablu u kojoj je upisan proizvoljni 
+#niz znakova i brojčanu varijablu n. Provjerite je li vrijednost varijable 
+#n manja od broja znakova u nizu. Ako je vrijednost varijable n veća 
+#ispišite informaciju o grešci. Ispišite iz niza znakova svako n-to 
+#slovo. Na primjer, ulazni niz je "ABCDEFGH", n je 2, tada je izlaz "ACEG"
+
+string=input("Unesi string: ")
+n= int(input("Unesi broj"))
+b=""
+
+print(len(string))
+if len(string) > n:
+    print("greška")
+for n in range(len(string)):
+   if (n % 2) == 0:
+      b += string[n]
+print(b)
+#%%
+#17 Napišite program koji sadrži varijablu u kojoj je upisan proizvoljni niz znakova. Ispišite koliko velikih slova se nalazi u nizu. Ako je neko od 
+#unesenih slova u nizu "A", brojanje velikih slova je potrebno prekinuti  i ispisati informaciju da je veliko slovo "A" pronađeno.
+
+string=(input("Unesi string"))
+
+velika=[char for char in string if char.isupper()]
+
+while True:
+    if "A" in string:
+        print("Pronaden je A")
+        break
+    else:
+        print(str(velika))
+#%%
+#18. Ispišite sve parne brojeve između 1 i 1000 koju su istovremeno djeljivi i s 5 i s 13.
+
+for i in range(1000):
+    if i%2==0 and i%5==0 and i%13==0:
+        print(i)
+#%%
+#19 Napisi program koji unosi n brojeva i od znamenke desetice svakog broja stvara novi broj.
 n=int(input("unesi bro:j"))
 g=""
 for i in range(n):
     a = int(input("unesi neki broj: "))
     g += str((a%100) // 10)
 print(g)
+
+#%%
+#20. Napisi program koji unosi n brojeva i sastavlja novi broj od najvece znamenke u svakom od brojeva 
+
+new_number=""
+while True:
+    n=(input("Unos: "))
+    if n=="0":
+        break
+
+    new_number+=max(n)
+print(new_number)
 #%%
 
+#21b
+a=1
+b=2
+while a<= 5 and b>=0:
+    print(b*" "+a*"*")
+    a+=2
+    b-=1
+    if a ==7 and b==-1:
+        a=3
+        b=1
+        while a >=1 and b<=2:
+            print(b*" "+a*"*")
+            b+=1
+            a-=2
+
+        break
 
 
 
+#%%
+#%%
+#22. Napišite program za koji traži najveći zajednički djelitelj dvaju brojeva
+
+def djelitelj(a, b):
+    if(b == 0):
+        return a
+    else:
+        return djelitelj(b, a % b)
+  
+a = int(input())
+b = int(input())
+  
+print("Najveci zajednicki djelitelj je ", end="")
+print(djelitelj(a,b))
+# %%
